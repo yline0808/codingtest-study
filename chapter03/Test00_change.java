6b5fa2260df813530e4c05b5d45804e21c2945bf
류요선 <ryuyoseon@rysMAC.local>
2021-05-26 23:44:41 +0900

    [add] chapter03 bigNumber create
    * 숫자를 더하여 가장 큰 수가 나오도록 하는 코드 계산식을 만들어 작성
    * 두번째 방법으로 단순하게 푸는 방법 시작 전
    * chapter 내부 title 파일 comment 파일로 이름 변경

---
+++ chapter03/Test00_change.java
@@ -0,0 +1,21 @@
+package chapter03;
+
+/**
+ * 거스름돈 걸러주기
+ * 
+ * 화폐의 종류가 K개라고 할 때 시간복잡도 O(K)
+ */
+public class Test00_change {
+    public static void main(String[] args) {
+        final int[] coin = { 500, 100, 50, 10 };
+        int change = 1260;
+        int cnt = 0;
+
+        for (int i = 0; i < coin.length; i++) {
+            cnt += change / coin[i];
+            change %= coin[i];
+        }
+
+        System.out.println(cnt);
+    }
+}
---
+++ chapter03/Test01_bigNumber.java
@@ -0,0 +1,57 @@
+package chapter03;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+/*
+ * 큰수의 법칙
+ * 
+ * 순서대로 N크기 배열이 주어지고 M번 더하기를 진행합니다. 단, 연속으로 K번만 더할 수 있습니다.
+ * 
+ * ===test1===
+ * 식을 만들어서 하는 방법
+ * (m / (k + 1)) * k + (m % (k + 1))
+ * (m / (k + 1))
+ * 
+ * ===test2===
+ * 기본적인 방법
+ */
+
+public class Test01_bigNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("배열의 크기N, 더하기 수M, 연속 수K, 숫자 배열을 입력 : ");
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int k = scanner.nextInt();
+
+        ArrayList arr = new ArrayList<Integer>();
+
+        for (int i = 0; i < n; i++) {
+            arr.add(scanner.nextInt());
+        }
+        scanner.close();
+
+        System.out.printf("%d %d %d\n", n, m, k);
+        System.out.println(arr.toString());
+        Collections.sort(arr, Collections.reverseOrder());
+        int bigNum = (int) arr.get(0);
+        int smallNum = (int) arr.get(1);
+
+        // =======test1=========
+        long startTime = System.currentTimeMillis();
+        int result = bigNum * (m / (k + 1)) * k + (m % (k + 1)) + smallNum * (m / (k + 1));
+        long endTime = System.currentTimeMillis();
+        System.out.println((endTime - startTime) + ":result : " + result);
+
+        // =======test2=========
+        result = 0;
+        startTime = System.currentTimeMillis();
+
+        endTime = System.currentTimeMillis();
+        System.out.println((endTime - startTime) + ":return : " + result);
+    }
+}
