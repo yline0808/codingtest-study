c4ff0f4f5cdf1315d1a534ed100168ba035f7f30
류요선 <ryuyoseon@rysMAC.local>
2021-06-12 12:51:51 +0900

    add programmers coding test
    * 타겟넘버 문제 해결
    * 기능개발 문제 해결중

---
+++ programmers/level2/FunctionDevelopment.java
@@ -0,0 +1,21 @@
+package programmers.level2;
+
+import mylib.MyLibs;
+
+/*
+ * 제목 : 기능개발
+ * url : https://programmers.co.kr/learn/courses/30/lessons/42586
+ * 유형 : 스택/큐
+ */
+
+public class FunctionDevelopment {
+    public static void main(String[] args) {
+        int[] progresses = { 93, 30, 55 };
+        int[] speeds = { 1, 30, 5 };
+        MyLibs.pList(solution(progresses, speeds));
+    }
+
+    public static int[] solution(int[] progresses, int[] speeds) {
+        return progresses;
+    }
+}
---
+++ programmers/level2/TargetNum.java
@@ -0,0 +1,29 @@
+package programmers.level2;
+
+/*
+ * 제목 : 타겟 넘버
+ * url : https://programmers.co.kr/learn/courses/30/lessons/43165
+ * 유형 : 깊이/너비 우선 탐색(DFS/BFS)
+ */
+
+public class TargetNum {
+    public static void main(String[] args) {
+        int[] numbers = { 1, 1, 1, 1, 1 };
+        int target = 3;
+
+        System.out.println(solution(numbers, target));
+    }
+
+    public static int solution(int[] numbers, int target) {
+        int answer = dfs(numbers, target, 0, 0);
+        return answer;
+    }
+
+    public static int dfs(int[] numbers, int target, int index, int num) {
+        if (numbers.length == index)
+            return target == num ? 1 : 0;
+        else
+            return dfs(numbers, target, index + 1, num + numbers[index])
+                    + dfs(numbers, target, index + 1, num - numbers[index]);
+    }
+}
